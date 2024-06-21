000faeeb4665264a29611b468498f0887c7fd2f1
Ashutosh-377 <acharyaashutosh377@gmail.com>
2024-06-21 11:59:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,130 @@
+var card1 = document.querySelectorAll("#one")
+var heading1 = document.querySelectorAll("#one .content h1")
+var icon1 = document.querySelectorAll("#one .icon i")
+
+card1.forEach(function(hi){
+    hi.addEventListener("mouseenter", function(){
+        heading1.forEach(function(hello){
+            hello.style.color = "#C80036"
+        })
+        icon1.forEach(function(i){
+            i.style.color = "#C80036"
+        })
+    })
+})
+
+card1.forEach(function(hi){
+    hi.addEventListener("mouseleave", function(){
+        heading1.forEach(function(hello){
+            hello.style.color = "#fff"
+        })
+        icon1.forEach(function(i){
+            i.style.color = "#fff"
+        })
+    })
+})
+
+var card2 = document.querySelectorAll("#two")
+var heading2 = document.querySelectorAll("#two .content h1")
+var icon2 = document.querySelectorAll("#two .icon i")
+
+card2.forEach(function(hi){
+    hi.addEventListener("mouseenter", function(){
+        heading2.forEach(function(hello){
+            hello.style.color = "#C80036"
+        })
+        icon2.forEach(function(i){
+            i.style.color = "#C80036"
+        })
+    })
+})
+
+card2.forEach(function(hi){
+    hi.addEventListener("mouseleave", function(){
+        heading2.forEach(function(hello){
+            hello.style.color = "#fff"
+        })
+        icon2.forEach(function(i){
+            i.style.color = "#fff"
+        })
+    })
+})
+
+var card3 = document.querySelectorAll("#three")
+var heading3 = document.querySelectorAll("#three .content h1")
+var icon3 = document.querySelectorAll("#three .icon i")
+
+card3.forEach(function(hi){
+    hi.addEventListener("mouseenter", function(){
+        heading3.forEach(function(hello){
+            hello.style.color = "#C80036"
+        })
+        icon3.forEach(function(i){
+            i.style.color = "#C80036"
+        })
+    })
+})
+
+card3.forEach(function(hi){
+    hi.addEventListener("mouseleave", function(){
+        heading3.forEach(function(hello){
+            hello.style.color = "#fff"
+        })
+        icon3.forEach(function(i){
+            i.style.color = "#fff"
+        })
+    })
+})
+
+var card4 = document.querySelectorAll("#four")
+var heading4 = document.querySelectorAll("#four .content h1")
+var icon4 = document.querySelectorAll("#four .icon i")
+
+card4.forEach(function(hi){
+    hi.addEventListener("mouseenter", function(){
+        heading4.forEach(function(hello){
+            hello.style.color = "#C80036"
+        })
+        icon4.forEach(function(i){
+            i.style.color = "#C80036"
+        })
+    })
+})
+
+card4.forEach(function(hi){
+    hi.addEventListener("mouseleave", function(){
+        heading4.forEach(function(hello){
+            hello.style.color = "#fff"
+        })
+        icon4.forEach(function(i){
+            i.style.color = "#fff"
+        })
+    })
+})
+
+var card5 = document.querySelectorAll("#five")
+var heading5 = document.querySelectorAll("#five .content h1")
+var icon5 = document.querySelectorAll("#five .icon i")
+
+card5.forEach(function(hi){
+    hi.addEventListener("mouseenter", function(){
+        heading5.forEach(function(hello){
+            hello.style.color = "#C80036"
+        })
+        icon5.forEach(function(i){
+            i.style.color = "#C80036"
+        })
+    })
+})
+
+card5.forEach(function(hi){
+    hi.addEventListener("mouseleave", function(){
+        heading5.forEach(function(hello){
+            hello.style.color = "#fff"
+        })
+        icon5.forEach(function(i){
+            i.style.color = "#fff"
+        })
+    })
+})
+
